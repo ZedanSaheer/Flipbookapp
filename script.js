22f9ba3713c765e752476c7d95a1fee9eb437c36
Zedan Saheer <zed.saheer5@gmail.com>
2021-06-26 20:37:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 // navbar function
-function myFunction(){
-const hmenu = document.querySelector(".navlinks");
-hmenu.classList.toggle("responsive")
+function myFunction() {
+    const hmenu = document.querySelector(".navlinks");
+    hmenu.classList.toggle("responsive")
 }
+
+//file input
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function () {
+    realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function () {
+    if (realFileBtn.value) {
+        customTxt.innerHTML = realFileBtn.value.match(
+            /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
+    } else {
+        customTxt.innerHTML = "No file chosen, yet.";
+    }
+});
+
+
+//button click loading
+const button = document.getElementById("button");
+const loading = document.getElementById("wrapperloading");
+button.addEventListener("click", activeLoad);
+
+function activeLoad() {
+    if ( customTxt.innerHTML = "No file chosen, yet.") {
+        alert("Add Some Document");
+    } else {
+        loading.classList.toggle("loadingactive");
+    }
+}
+
